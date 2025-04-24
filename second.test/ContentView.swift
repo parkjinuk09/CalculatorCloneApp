c8b7e57f5e709ed951a1dc806da3359d39bb8a19
parkjinuk09 <parkjinuk09@gmail.com>
2025-04-24 17:58:30 +0900

    first push

---
+++ second.test/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  second.test
+//
+//  Created by 박진욱 on 4/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showSheet = false
+    var body: some View {
+        Navigationview{
+            List
+        }
+    }
+}
+
+
+
+#Preview {
+    ContentView()
+}
---
+++ second.test/second_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  second_testApp.swift
+//  second.test
+//
+//  Created by 박진욱 on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct second_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
